fcbb77bb949f86fa2bf9d3a12adf89d4c406076a
Sowon <cherishwish12@gmail.com>
2024-01-24 00:01:25 +0900

    0123

--- slvd/class3/0123-4949.py
+++ slvd/class3/0123-4949.py
@@ -1,22 +1,28 @@
 #4949 균형잡힌 세상
 while(True):
-    a = input()
-    f1 = 0
-    f2 = 0
-    if a =='.':
-        print('Yes')
+    line=input()
+    line_1=list(line)
+    stack=[]
+    temp=True
+
+    if(line=='.'):
         break
+    for i in line_1:
+        if(i=='(' or i=='['):
+            stack.append(i)
+        elif(i==')'):
+            if not stack or stack[-1]=='[':
+                temp=False
+                break
+            elif stack[-1]=='(':
+                stack.pop()
+        elif(i==']'):
+            if not stack or stack[-1]=='(':
+                temp=False
+                break
+            elif stack[-1]=='[':
+                stack.pop()
+    if temp==True and not stack:
+        print("yes")
     else:
-        for word in a:
-            if word=='(':
-                f1+=1
-            if word==')':
-                f1-=1
-            if word=='[':
-                f2+=1
-            if word==']':
-                f2-=1
-        if f1==0 and f2==0:
-            print('Yes')
-        else:
-            print('No')
+        print("no")
